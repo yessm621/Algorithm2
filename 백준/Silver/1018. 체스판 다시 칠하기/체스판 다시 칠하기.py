1d69e35866f4b08d1789b2780e6d0c560307ea40
yessm621 <yessm621@gmail.com>
2023-04-28 13:50:28 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 84 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+
+board = []
+for _ in range(n):
+    board.append(input())
+
+result = []
+for a in range(n-7):
+    for b in range(m-7):
+        index1 = 0
+        index2 = 0
+        for i in range(a, a+8):
+            for j in range(b, b+8):
+                if (i+j) % 2 == 0:
+                    if board[i][j] != 'W':
+                        index1 += 1
+                    if board[i][j] != 'B':
+                        index2 += 1
+                else:
+                    if board[i][j] != 'B':
+                        index1 += 1
+                    if board[i][j] != 'W':
+                        index2 += 1
+        result.append(min(index1, index2))
+print(min(result))
