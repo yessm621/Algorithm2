7aeceeea97463a119767f00d8b65b86386487996
yessm621 <yessm621@gmail.com>
2023-05-15 10:47:33 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 1164 ms, Memory: 41736 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -0,0 +1,23 @@
+import sys
+
+prime = []
+check = [0] * 1000001
+check[0] = 1
+check[1] = 1
+for i in range(2, 1000001):
+    if check[i] == 0:
+        prime.append(i)
+        for j in range(2*i, 1000001, i):
+            check[j] = 1
+
+t = int(input())
+
+for _ in range(t):
+    count = 0
+    N = int(sys.stdin.readline())
+    for i in prime:
+        if i >= N:
+            break
+        if not check[N-i] and i <= N-i:
+            count += 1
+    print(count)
