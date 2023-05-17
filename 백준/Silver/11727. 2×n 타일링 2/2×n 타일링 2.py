c2f1662f3e58a6b6a95ff037af834fc87353182d
yessm621 <yessm621@gmail.com>
2023-05-17 12:49:06 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 31432 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,15 @@
+def dp(x):
+    if x == 1:
+        return 1
+    if x == 2:
+        return 3
+    if d[x] != 0:
+        return d[x]
+    
+    d[x] = (dp(x-1) + 2 * dp(x-2)) % 10007
+    return d[x]
+
+n = int(input())
+d = [0] * (n + 1)
+
+print(dp(n))
