07cdc3f03384670857fe8615b5340612af278422
yessm621 <yessm621@gmail.com>
2024-05-28 22:58:42 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,15 +1,7 @@
-def dp(x):
-    if x == 1:
-        return 1
-    if x == 2:
-        return 2
-    if d[x] != 0:
-        return d[x]
-    
-    d[x] = (dp(x-1) + dp(x-2)) % 10007
-    return d[x]
-
 n = int(input())
-d = [0] * (n + 1)
+arr = [0, 1, 2]
 
-print(dp(n))
+for i in range(3, n+1):
+    arr.append(arr[i-1] + arr[i-2])
+    
+print(arr[n] % 10007)
